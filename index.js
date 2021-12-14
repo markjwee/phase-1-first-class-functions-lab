ba8e8d54df4bdcb99af06b9a7abbc158a9545e02
Mark <mark.j.wee@gmail.com>
2021-12-14 11:46:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+// const drivers = ["Antonia", "Nuru", "Amari", "Claudia"]
+
+// const returnFirstTwoDrivers = (listOfDrivers) => {
+//     return [...listOfDrivers.slice(0, 2)];
+// }
+
+// function returnFirstTwoDrivers(passedInArray) {
+//     // if (Array.isArray(passedInArray))
+//         return passedInArray.slice(0, 2);
+// }
+
+// function returnLastTwoDrivers(passedInArray) {
+//     // if (Array.isArray(passedInArray))
+//     return passedInArray.slice(-0, -3);
+// }
+
+const returnFirstTwoDrivers = function(passedInArray){return passedInArray.slice(0,2)}
+
+const returnLastTwoDrivers = function(passedInArray){return passedInArray.slice(-2)}
+
+
+const selectingDrivers = function(x){return }
+[...returnFirstTwoDrivers, ...returnLastTwoDrivers]
+ 
